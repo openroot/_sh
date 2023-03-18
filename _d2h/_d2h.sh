13392459cc0cb5bd96a6793b61becc91a3a796b6
openroot <dev.openroot@gmail.com>
2023-03-18 16:54:28 +0530

    temp progress

--- _d2h/_d2h.sh
+++ _d2h/_d2h.sh
@@ -10,19 +10,29 @@ function _trap() {
 	exit; # exit the app (on next run) pressing <<ctrl><c>>
 }
 
-function _tempfn() {
+function _entry() {
+	local _arg1=$1;
 	_fs_const_dir=`dirname $0`; # current directory (relative)
 
 	_channellist_json_file="$_fs_const_dir/packs/channel-list.json";
 
-	_numberofenlist=`cat $_channellist_json_file | jshon -l`;
+	local _numberofenlist=`cat $_channellist_json_file | jshon -l`;
 	printf "Number of enlist(s): $_numberofenlist\n";
+
+	local _isfound=-1;
+
+	# trying taking channel selection from passed argument from console
+	if ! [ -z $_arg1 ]
+	then
+		printf "Passed argument: $_arg1\n";
+	fi
 }
 
 # endregion
 
 # region execute
 
-_tempfn;
+_arg1=$1;
+_entry $_arg1;
 
 # endregion
